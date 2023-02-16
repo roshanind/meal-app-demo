11806b85e9d1e476235f26c4992143bf1aac2c13
roshanind <roshan@smashtaps.com>
2023-02-16 11:36:58 +0530

    Initial commit

---
+++ test/meal-app-demo.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as MealAppDemo from '../lib/meal-app-demo-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/meal-app-demo-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new MealAppDemo.MealAppDemoStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
